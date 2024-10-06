1cd2d52162ea002e5de5994c8f5a64ddef86b785
roel <roel@contigo.im>
2024-10-06 21:15:47 +0900

    [Add] Wavve app 전용 컬러 추가

--- app/src/main/java/org/sopt/and/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/and/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Wavve 전용 색상
+val BlueBtnColor = Color(0xFF1351F9)
+val darkGray1 = Color(0xFF252525)
+val darkGray2 = Color(0xFF2F2F2F)
+val darkGray3 = Color(0xFFA5A5A5)
