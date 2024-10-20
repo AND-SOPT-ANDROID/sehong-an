390a74e4e57826f2bc4a6795276d8e3444bf285b
roel <ash49901@gmail.com>
2024-10-20 21:39:42 +0900

    [ADD] NetworkModule 을 통해 Hilt를 통해 의존성을 관리하고 주입

---
+++ app/src/main/java/org/sopt/and/data/NetworkModule.kt
@@ -0,0 +1,25 @@
+package org.sopt.and.data
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager {
+        return PreferencesManager(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserManager(preferencesManager: PreferencesManager): UserManager {
+        return UserManager(preferencesManager)
+    }
+}
