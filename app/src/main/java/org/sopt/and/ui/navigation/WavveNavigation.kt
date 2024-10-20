b20b177444eecf58b7d15190cfc50abeb26f5ef3
roel <ash49901@gmail.com>
2024-10-20 21:43:26 +0900

    [MOD] 불필요한 코드 삭제

--- app/src/main/java/org/sopt/and/ui/navigation/WavveNavigation.kt
+++ app/src/main/java/org/sopt/and/ui/navigation/WavveNavigation.kt
@@ -4,11 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import org.sopt.and.data.PreferencesManager
 import org.sopt.and.data.UserManager
 import org.sopt.and.ui.screen.myPage.composable.MyScreen
 import org.sopt.and.ui.screen.signIn.composable.SignInScreen
@@ -20,9 +18,6 @@ fun WavveNavigation(
 ) {
     val navController = rememberNavController()
     val startDestination = remember { mutableStateOf("signIn") }
-    val context = LocalContext.current
-    val preferencesManager = PreferencesManager(context)
-    val userManager = UserManager(preferencesManager)
     /** 로그인 상태에 따라 시작 화면을 결정 */
     LaunchedEffect(key1 = true) {
         startDestination.value = if (userManager.isLoggedIn()) "myPage" else "signIn"
