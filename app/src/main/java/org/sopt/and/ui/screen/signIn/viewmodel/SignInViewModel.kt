88121f8f3c252d76a343e39b9f2c82e702d55bcc
roel <ash49901@gmail.com>
2024-10-24 01:55:54 +0900

    [MOD] SignInScreen 및 SignInViewModel navigation 설정에 따라 추가

--- app/src/main/java/org/sopt/and/ui/screen/signIn/viewmodel/SignInViewModel.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signIn/viewmodel/SignInViewModel.kt
@@ -1,10 +1,56 @@
 package org.sopt.and.ui.screen.signIn.viewmodel
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.sopt.and.data.UserManager
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor() : ViewModel() {
+class SignInViewModel @Inject constructor(
+    private val userManager: UserManager
+) : ViewModel() {
+    var userIdInput by mutableStateOf("")
+        private set
+    var passwordInput by mutableStateOf("")
+        private set
+    var showDialog by mutableStateOf(false)
+        private set
 
+    fun onUserIdInputChange(newInput: String) {
+        userIdInput = newInput
+    }
+
+    fun onPasswordInputChange(newInput: String) {
+        passwordInput = newInput
+    }
+
+    fun handleLoginClick(navController: NavHostController, context: Context) {
+        if (userIdInput.isEmpty() || passwordInput.isEmpty()) {
+            showDialog = true
+            return
+        }
+        val login = userManager.loginUser(userIdInput, passwordInput)
+        if (login) {
+            Toast.makeText(context, "로그인 성공", Toast.LENGTH_SHORT).show()
+            userManager.setLoggedIn(true)  // 자동 로그인 설정
+            navController.navigate("my") {
+                popUpTo(navController.graph.startDestinationId) {
+                    inclusive = true
+                }
+                launchSingleTop = true
+            }
+        } else {
+            showDialog = true
+        }
+    }
+
+    fun dismissDialog() {
+        showDialog = false
+    }
 }
