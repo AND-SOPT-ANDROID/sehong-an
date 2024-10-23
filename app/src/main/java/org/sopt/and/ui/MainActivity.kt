96bf2cce7e8a9e6dd3c0852fb50b8f14f27e8eab
roel <ash49901@gmail.com>
2024-10-24 01:56:37 +0900

    [MOD] navigation 및 SAA 설정에 따른 MainActivity 수정

--- app/src/main/java/org/sopt/and/ui/MainActivity.kt
+++ app/src/main/java/org/sopt/and/ui/MainActivity.kt
@@ -3,10 +3,21 @@ package org.sopt.and.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.and.data.UserManager
-import org.sopt.and.navigation.WavveNavigation
+import org.sopt.and.navigation.BottomNavItem
+import org.sopt.and.navigation.BottomNavigation
+import org.sopt.and.navigation.MainNavigation
+import org.sopt.and.topbar.TopBar
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 import javax.inject.Inject
 
@@ -18,8 +29,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ANDANDROIDTheme {
-                Surface {
-                    WavveNavigation(userManager = userManager)
+                val navController = rememberNavController()
+                val screens = listOf(
+                    BottomNavItem.Home.route,
+                    BottomNavItem.Search.route,
+                    BottomNavItem.My.route,
+                )
+                val currentRoute =
+                    navController.currentBackStackEntryAsState().value?.destination?.route
+                val showBottomNav = navController
+                    .currentBackStackEntryAsState().value?.destination?.route in screens.map { it }
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    topBar = { TopBar(navController, currentRoute) },
+                    bottomBar = {
+                        AnimatedVisibility(
+                            visible = showBottomNav,
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            BottomNavigation(navController = navController)
+                        }
+                    }
+                ) { innerPadding ->
+                    // Scaffold의 패딩을 content에 적용
+                    MainNavigation(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
