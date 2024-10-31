eb1da87982dbd5bc8e9bac1a7541bf1910b710af
roel <ash49901@gmail.com>
2024-11-01 01:52:37 +0900

    [MOD] MainScreen 분리

--- app/src/main/java/org/sopt/and/ui/MainActivity.kt
+++ app/src/main/java/org/sopt/and/ui/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -28,38 +26,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ANDANDROIDTheme {
-                val navController = rememberNavController()
-                val screens = listOf(
-                    BottomNavItem.Home.route,
-                    BottomNavItem.Search.route,
-                    BottomNavItem.My.route,
-                )
-                val currentRoute =
-                    navController.currentBackStackEntryAsState().value?.destination?.route
-                val showBottomNav = navController
-                    .currentBackStackEntryAsState().value?.destination?.route in screens.map { it }
-                Scaffold(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    topBar = { TopBar(navController, currentRoute) },
-                    bottomBar = {
-                        AnimatedVisibility(
-                            visible = showBottomNav,
-                            enter = fadeIn(),
-                            exit = fadeOut()
-                        ) {
-                            BottomNavigation(navController = navController)
-                        }
-                    }
-                ) { innerPadding ->
-                    // Scaffold의 패딩을 content에 적용
-                    MainNavigation(
-                        navController = navController,
-                        modifier = Modifier.padding(innerPadding),
-                        userManager = viewModel.userManager
-                    )
-                }
+                MainScreen(viewModel)
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen(viewModel: MainViewModel) {
+    val navController = rememberNavController()
+    val screens = listOf(
+        BottomNavItem.Home.route,
+        BottomNavItem.Search.route,
+        BottomNavItem.My.route,
+    )
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+    val showBottomNav = currentRoute in screens
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopBar(navController, currentRoute) },
+        bottomBar = {
+            if (showBottomNav) {
+                BottomNavigation(navController = navController)
+            }
+        }
+    ) { innerPadding ->
+        // Scaffold의 패딩을 content에 적용
+        MainNavigation(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding),
+            userManager = viewModel.userManager
+        )
     }
 }
