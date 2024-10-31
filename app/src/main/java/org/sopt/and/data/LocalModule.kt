6a5161a3a9c0e08f87e62351486a781181bb4d25
roel <ash49901@gmail.com>
2024-10-31 12:14:57 +0900

    [MOD] NetworkModule => LocalModule 로 이름 수정

--- app/src/main/java/org/sopt/and/data/LocalModule.kt
+++ app/src/main/java/org/sopt/and/data/LocalModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
+object LocalModule {
     @Provides
     @Singleton
     fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager {
