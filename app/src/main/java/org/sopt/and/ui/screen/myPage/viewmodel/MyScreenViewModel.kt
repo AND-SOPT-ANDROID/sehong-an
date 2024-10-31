b913eee2fa0033fd17a340de3143a8d8b563fef5
roel <ash49901@gmail.com>
2024-10-31 12:30:28 +0900

    [MOD] MyScreenViewModel 코드 간결화 진행 및 viewModelScope.launch(코루틴) 불필요 코드 수정

--- app/src/main/java/org/sopt/and/ui/screen/myPage/viewmodel/MyScreenViewModel.kt
+++ app/src/main/java/org/sopt/and/ui/screen/myPage/viewmodel/MyScreenViewModel.kt
@@ -1,11 +1,9 @@
 package org.sopt.and.ui.screen.myPage.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 import org.sopt.and.data.UserManager
 import javax.inject.Inject
 
@@ -13,23 +11,11 @@ import javax.inject.Inject
 class MyScreenViewModel @Inject constructor(
     private val userManager: UserManager
 ) : ViewModel() {
-    private val _profileName = MutableStateFlow("프로필1")
+    private val _profileName = MutableStateFlow(userManager.getUserEmail() ?: "프로필1")
     val profileName: StateFlow<String> = _profileName
 
-    init {
-        loadUserProfile()
-    }
-
-    private fun loadUserProfile() {
-        viewModelScope.launch {
-            _profileName.value = userManager.getUserEmail() ?: "프로필1"
-        }
-    }
-
     fun logout() {
-        viewModelScope.launch {
-            userManager.logoutUser()
-            userManager.setLoggedIn(false)
-        }
+        userManager.logoutUser()
+        userManager.setLoggedIn(false)
     }
 }
