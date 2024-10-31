fb1bc7ea1e5590dcfa7344e184b9e609ee01e887
roel <ash49901@gmail.com>
2024-10-31 12:09:48 +0900

    [MOD] AndSopt.kt 사용하지 않는 코드 삭제

--- app/src/main/java/org/sopt/and/AndSopt.kt
+++ app/src/main/java/org/sopt/and/AndSopt.kt
@@ -1,18 +1,7 @@
 package org.sopt.and
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AndSopt : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        appContext = applicationContext
-    }
-
-    companion object {
-        lateinit var appContext: Context
-            private set
-    }
-}
+class AndSopt : Application()
