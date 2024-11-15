cb36e3c38a2f59bb13a9516b0f0b80baabfc5ac4
roel <ash49901@gmail.com>
2024-11-15 12:19:04 +0900

    Mod : HomeScreen string 처리

--- app/src/main/java/org/sopt/and/ui/components/lazyRow/EditorSelectImageLazyList.kt
+++ app/src/main/java/org/sopt/and/ui/components/lazyRow/EditorSelectImageLazyList.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,7 +42,7 @@ fun EditorSelectImageLazyList(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "믿고 보는 웨이브 에디터 추천작",
+            text = stringResource(id = R.string.wavve_editor_suggestion),
             style = WavveTheme.typography.title.copy(
                 color = WavveTheme.colors.white
             ),
--- app/src/main/java/org/sopt/and/ui/components/lazyRow/TopTwentyImageLazyList.kt
+++ app/src/main/java/org/sopt/and/ui/components/lazyRow/TopTwentyImageLazyList.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,7 +47,7 @@ fun TopTwentyImageList(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = "오늘의 TOP 20",
+                text = stringResource(id = R.string.today_top_twenty),
                 style = WavveTheme.typography.title.copy(
                     color = WavveTheme.colors.white
                 ),
