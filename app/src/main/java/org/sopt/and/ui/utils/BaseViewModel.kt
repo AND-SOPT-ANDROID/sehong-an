9d01bab7a1a8cb9ed8d5447137484e7f60e04695
roel <ash49901@gmail.com>
2024-12-01 17:43:35 +0900

    Mod : BaseViewModel 수정

--- app/src/main/java/org/sopt/and/ui/utils/BaseViewModel.kt
+++ app/src/main/java/org/sopt/and/ui/utils/BaseViewModel.kt
@@ -34,7 +34,7 @@ abstract class BaseViewModel<State : UiState, Event : UiEvent, Effect : UiEffect
     val uiState = _uiState.asStateFlow()
 
     private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
-    val event = _event.asSharedFlow()
+    private val event = _event.asSharedFlow()
 
     private val _effect: Channel<Effect> = Channel()
     val effect = _effect.receiveAsFlow()
